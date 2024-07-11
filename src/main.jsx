7f1d1a7120519685fcfc180f6cf1357d81286820
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-11 23:16:05 +0500

    adding line

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import VideoCallingApp from './VideoCallingApp.jsx'
 
-//removing this line for git activity
+//adding this line for git activity
 
 const queryClient = new QueryClient();
 
