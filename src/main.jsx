87faaf74899c648d01061090374e61b0711dc9de
Noor ur Rehman <135664580+TheRehmandev@users.noreply.github.com>
2024-07-25 23:28:32 +0500

    Commiting changes for git activity

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import VideoCallingApp from './VideoCallingApp.jsx'
 
-//changing this line for git activity
+//Adding this line for git activity
 
 const queryClient = new QueryClient();
 
