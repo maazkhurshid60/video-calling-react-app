91342610423a46d3fc38fa0fdbf08c27f8a65051
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-01-09 20:50:58 +0500

    git activity performed:mudasser

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import VideoCallingApp from './VideoCallingApp.jsx'
 
-//Added this line for git activity
+//Deleting this line for git activity
 
 const queryClient = new QueryClient();
 
